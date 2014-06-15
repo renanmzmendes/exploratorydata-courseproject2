75a225a8476ba63e2f78b22c652ea2726fd7ebf7
Renan Mendes <renanmzmendes@gmail.com>
2014-06-15 08:59:59 -0300

    adding messages to plot scripts

--- plot1.R
+++ plot1.R
@@ -1,7 +1,9 @@
 # Reading data
+message("Reading data")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+message("Starting plot")
 png("plot1.png")
 par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1)
 emissions <- tapply(NEI$Emissions, as.factor(NEI$year), sum)
@@ -9,3 +11,4 @@ plot(levels(as.factor(NEI$year)), emissions, type="l",
       xlab = "Year", ylab = "Emissions (tons)", 
       main = "Emissions per year in the United States")
 dev.off()
+message("Plot finished")
--- plot2.R
+++ plot2.R
@@ -1,7 +1,9 @@
 # Reading data
+message("Reading data")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+message("Starting plot")
 png("plot2.png")
 par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1)
 baltimore <- NEI[NEI$fips == "24510",]
@@ -10,3 +12,4 @@ plot(levels(as.factor(baltimore$year)), emissions, type="l",
      xlab = "Year", ylab = "Emissions (tons)", 
      main = "Emissions per year in Baltimore")
 dev.off()
+message("Plot finished")
